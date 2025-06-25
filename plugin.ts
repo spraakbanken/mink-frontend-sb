e1afb5abd534588d44479a301d0e64dcfbcf4937
Arild Matsson <arild.matsson@gu.se>
2025-06-25 14:18:13 +0200

    Tamper with locale

--- plugin.ts
+++ plugin.ts
@@ -1,8 +1,25 @@
 import type { Plugin } from "vue";
 import SbMinkLogo from "./components/SbMinkLogo.vue";
+import i18n, { languageNames } from "@/i18n/i18n";
 
 export default function createPlugin(): Plugin {
   return (app) => {
+    // Override an overridable component
     app.provide("MinkLogo", SbMinkLogo);
+
+    // Prefer Swedish if it's among browser's preferred languages, even if English is ranked higher
+    const isSvPreferred = navigator.languages.find((l) => /^sv\b/.test(l));
+    i18n.global.locale.value = isSvPreferred ? "sv" : "en";
+
+    // Modify a language
+    import("@instance/locales/sv.yaml").then((module) => {
+      i18n.global.mergeLocaleMessage("sv", module.default);
+    });
+
+    // Add a language
+    import("@instance/locales/es.yaml").then((module) => {
+      i18n.global.setLocaleMessage("es", module.default);
+      languageNames.es = "Español";
+    });
   };
 }
