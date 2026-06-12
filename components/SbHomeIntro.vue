4a58eaf3c711932092021cc4cb80a3c42eff6f3d
Arild Matsson <arild.matsson@gu.se>
2026-06-12 13:41:54 +0200

    Rewrite homepage

--- components/SbHomeIntro.vue
+++ components/SbHomeIntro.vue
@@ -1,20 +1,34 @@
 <template>
   <div>
+    <h2 class="mb-4 text-2xl font-heading">
+      {{ $t("home.hero.subtitle") }}
+    </h2>
+
     <i18n-t keypath="home.hero.sb" scope="global" tag="p" class="my-2">
-      <strong>Språkbanken Text</strong>
+      <a :href="$t('links.sbx.url')" target="_blank">Språkbanken</a>
     </i18n-t>
+
     <i18n-t keypath="home.hero.mink" scope="global" tag="p" class="my-2">
       <strong>Mink</strong>
     </i18n-t>
-    <i18n-t keypath="home.hero.privacy" scope="global" tag="p" class="my-2">
-      <a :href="$t('home.hero.privacy.link.url')">
-        {{ $t("home.hero.privacy.link.label") }}
-      </a>
-    </i18n-t>
-    <i18n-t keypath="home.hero.readmore" scope="global" tag="p" class="my-2">
-      <a :href="$t('docs.url')" target="_blank">
-        {{ $t("home.hero.readmore.link.label") }}
-      </a>
-    </i18n-t>
+
+    <p>{{ $t("home.hero.workflow") }}</p>
+
+    <ul class="list-disc my-2 ms-6">
+      <li>
+        <i18n-t keypath="home.hero.workflow.create" scope="global">
+          <template #corpora>
+            <strong>{{ $t("home.hero.workflow.create.corpora") }}</strong>
+          </template>
+          <template #lexicons>
+            <strong>{{ $t("home.hero.workflow.create.lexicons") }}</strong>
+          </template>
+        </i18n-t>
+      </li>
+      <li>{{ $t("home.hero.workflow.run") }}</li>
+      <li>{{ $t("home.hero.workflow.download") }}</li>
+      <li>{{ $t("home.hero.workflow.install") }}</li>
+      <li>{{ $t("home.hero.workflow.invite") }}</li>
+    </ul>
   </div>
 </template>
--- components/SbHomeSecondary.vue
+++ components/SbHomeSecondary.vue
@@ -1,129 +1,127 @@
 <script setup lang="ts">
+import CardBox from "@/components/CardBox.vue";
 import LayoutSection from "@/components/LayoutSection.vue";
+import {
+  PhBookOpenText,
+  PhBracketsCurly,
+  PhFolderOpen,
+  PhListMagnifyingGlass,
+  PhLockKeyOpen,
+  PhMagicWand,
+} from "@phosphor-icons/vue";
 </script>
 
 <template>
   <div>
-    <div
-      class="flex flex-col 2xl:flex-row my-20 gap-20 md:w-4/5 lg:w-2/3 2xl:w-full mx-auto"
+    <!-- Features -->
+    <h2 class="text-3xl text-center mb-8">{{ $t("home.features.title") }}</h2>
+    <ul
+      class="my-8 max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-4 text-lg"
     >
-      <div
-        class="flex-1 flex flex-col md:flex-row 2xl:flex-col items-center gap-6"
-      >
-        <figure
-          class="md:w-1/2 2xl:w-4/5 shadow-lg hover:scale-110 transition-transform"
+      <CardBox tag="li" class="flex-1 flex gap-4 items-center">
+        <PhFolderOpen class="w-6 h-6 shrink-0" />
+        {{ $t("home.features.upload") }}
+      </CardBox>
+      <CardBox tag="li" class="flex-1 flex gap-4 items-center">
+        <PhMagicWand class="w-6 h-6 shrink-0" />
+        {{ $t("home.features.annotations") }}
+      </CardBox>
+      <CardBox tag="li" class="flex-1 flex gap-4 items-center">
+        <PhListMagnifyingGlass class="w-6 h-6 shrink-0" />
+        {{ $t("home.features.install") }}
+      </CardBox>
+      <CardBox tag="li" class="flex-1 flex gap-4 items-center">
+        <PhBookOpenText class="w-6 h-6 shrink-0" />
+        {{ $t("home.features.lexicons") }}
+      </CardBox>
+    </ul>
+
+    <!-- Meta-features -->
+    <ul
+      class="my-16 max-w-4xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-4 text-lg"
+    >
+      <CardBox tag="li" class="flex-1">
+        <div class="text-center mb-4">
+          <PhLockKeyOpen class="inline w-8 h-8" />
+        </div>
+        {{ $t("home.features.privacy") }}
+      </CardBox>
+
+      <CardBox tag="li" class="flex-1">
+        <div class="text-center mb-4">
+          <PhBracketsCurly class="inline w-8 h-8" />
+        </div>
+        <i18n-t keypath="home.features.code" scope="global">
+          <template #repos>
+            <a href="https://github.com/spraakbanken/?q=mink" target="_blank">
+              {{ $t("home.features.code.repos") }}
+            </a>
+          </template>
+        </i18n-t>
+      </CardBox>
+    </ul>
+
+    <!-- Links -->
+    <h2 class="text-3xl text-center mb-8">{{ $t("home.links.title") }}</h2>
+    <ul class="my-8 max-w-md mx-auto flex flex-col gap-4 text-lg">
+      <li>
+        <a :href="$t('home.links.tutorial.url')" target="_blank">
+          <strong>{{ $t("home.links.tutorial.label") }}</strong>
+        </a>
+        – {{ $t("home.links.tutorial") }}
+      </li>
+
+      <li>
+        <a
+          href="https://spraakbanken.gu.se/en/tools/mink/manual"
+          target="_blank"
         >
-          <img
-            src="../img/sparv-screen.png"
-            :alt="$t('home.features.sparv.alt')"
-          />
-        </figure>
+          <strong>{{ $t("home.links.manual.label") }}</strong>
+        </a>
+        – {{ $t("home.links.manual") }}
+      </li>
 
-        <LayoutSection
-          :title="$t('home.features.sparv.title')"
-          class="flex-1 my-0 text-center"
+      <li>
+        <a
+          href="https://spraakbanken.gu.se/en/tools/mink/data-policy"
+          target="_blank"
         >
-          <i18n-t keypath="home.features.sparv.body" scope="global" tag="p">
-            <template #sparv>
-              <a :href="$t('sparv.url')"><strong>Sparv</strong></a>
-            </template>
-          </i18n-t>
-        </LayoutSection>
-      </div>
+          <strong>{{ $t("home.links.privacy.label") }}</strong>
+        </a>
+      </li>
 
-      <div
-        class="flex-1 flex flex-col md:flex-row-reverse 2xl:flex-col items-center gap-6"
-      >
-        <figure
-          class="md:w-1/2 2xl:w-4/5 shadow-lg hover:scale-110 transition-transform"
-        >
-          <img src="../img/texts.png" :alt="$t('home.features.upload.alt')" />
-        </figure>
+      <li>
+        <a :href="$t('home.links.api.url')" target="_blank">
+          <strong>{{ $t("home.links.api.label") }}</strong>
+        </a>
+        – {{ $t("home.links.api") }}
+      </li>
 
-        <LayoutSection
-          :title="$t('home.features.upload.title')"
-          class="flex-1 my-0 text-center"
-        >
-          <i18n-t keypath="home.features.upload.body" scope="global" tag="p" />
+      <li>
+        <a :href="$t('home.links.resources.url')" target="_blank">
+          <strong>{{ $t("home.links.resources.label") }}</strong>
+        </a>
+        – {{ $t("home.links.resources") }}
+      </li>
+    </ul>
 
-          <i18n-t keypath="home.features.upload.xml" scope="global" tag="p">
-            <template #link>
-              <a
-                :href="$t('home.features.upload.xml.link.url')"
-                target="_blank"
-              >
-                {{ $t("home.features.upload.xml.link.label") }}
-              </a>
-            </template>
-          </i18n-t>
-        </LayoutSection>
-      </div>
-
-      <div
-        class="flex-1 flex flex-col md:flex-row 2xl:flex-col items-center gap-6"
-      >
-        <figure
-          class="md:w-1/2 2xl:w-4/5 shadow-lg hover:scale-110 transition-transform"
-        >
-          <img
-            src="../img/korp-screen.png"
-            :alt="$t('home.features.explore.alt')"
-          />
-        </figure>
-
-        <LayoutSection
-          :title="$t('home.features.explore.title')"
-          class="flex-1 my-0 text-center"
-        >
-          <i18n-t keypath="home.features.explore.body" scope="global" tag="p">
-            <template #korp>
-              <a
-                href="https://spraakbanken.gu.se/en/tools/korp"
-                title="About the Korp tool"
-              >
-                <strong>Korp</strong>
-              </a>
-            </template>
-            <template #strix>
-              <a href="https://spraakbanken.gu.se/strix/">
-                <strong>Strix</strong></a
-              >
-            </template>
-          </i18n-t>
-        </LayoutSection>
-      </div>
-
-      <div
-        class="flex-1 flex flex-col md:flex-row-reverse 2xl:flex-col items-center gap-6"
-      >
-        <figure
-          class="md:w-1/2 2xl:w-4/5 shadow-lg hover:scale-110 transition-transform"
-        >
-          <img src="../img/share.png" :alt="$t('home.features.share.alt')" />
-        </figure>
-
-        <LayoutSection
-          :title="$t('home.features.share.title')"
-          class="flex-1 my-0 text-center"
-        >
-          <p>{{ $t("home.features.share.body") }}</p>
-        </LayoutSection>
-      </div>
-    </div>
-
+    <!-- Mink or no Mink -->
     <div>
-      <p class="max-w-2xl mx-auto mb-2">
+      <p class="max-w-2xl mx-auto mt-16">
         <strong>{{ $t("home.otherdata.title") }}</strong>
-        {{ " " }}
+        {{}}
         <i18n-t keypath="home.otherdata.body" scope="global">
           <a
             :href="$t('home.otherdata.sbdata.url')"
             :title="$t('home.otherdata.sbdata.title')"
-            >Data</a
+            target="_blank"
           >
+            {{ $t("home.otherdata.sbdata.label") }}
+          </a>
           <a
             href="https://vlo.clarin.eu/"
             title="CLARIN Virtual Language Observatory"
+            target="_blank"
             >Virtual Language Observatory</a
           >
         </i18n-t>
